32a6187cc3125108c678650c7ff1cbd7596128d1
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-12-24 17:07:18 +0530

    Terraform Practice

---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket = "terraform-rs-multi-env-prod-bucket"
+key    = "tfvars-multi-env-demo"
+region = "us-east-1"
+use_lockfile = true
+encrypt = true
---
+++ tfvars/vars.tf
@@ -0,0 +1,53 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "project" {
+    type = string
+    default = "roboshop"
+}
+
+variable "sg_name" {
+    type = string
+    default = "allow-all"
+    description = "Security Group Name to attach with ec2 instance"
+}
+
+variable "cidr" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
+
+variable "ingress_from_port" {
+    default = 0
+}
+
+variable "ingress_to_port" {
+    default = 0
+}
+
+variable "egress_from_port" {
+    default = 0
+}
+
+variable "egress_to_port" {
+    default = 0
+}
+
+variable "protocol" {
+    default = "-1"
+}
+
+# variable "console_input" {
+#     type = list(string)
+#     description = "Enter the names you want to add: "
+# }
